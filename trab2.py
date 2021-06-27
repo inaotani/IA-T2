251a5cef04eb483dfa6870f99b1c009e4d1fda08
Ingrid Otani <ingrid.otani@ifood.com.br>
2021-06-27 20:10:54 -0300

    some test code and BFS code

--- trab2.py
+++ trab2.py
@@ -1,5 +1,7 @@
 import random
 import math
+import time
+
 
 def create_graph (v, k):
     list_vertices = []
@@ -7,14 +9,16 @@ def create_graph (v, k):
     for i in range(v):
         x = random.randint(0,v)
         y = random.randint(0,v)
-        list_vertices.append((x,y))
+        if (x,y) not in list_vertices:
+            list_vertices.append((x,y))
 
     for vertex in list_vertices:
         low_distances = []
         count = 0
         for aux_vertex in list_vertices:
-            distance = math.sqrt(math.pow((vertex[0] - aux_vertex[0]), 2) + math.pow((vertex[1] - aux_vertex[1]), 2))
-            low_distances.append((distance, vertex, aux_vertex))
+            if(vertex != aux_vertex):
+                distance = '{0:.3g}'.format(math.sqrt(math.pow((vertex[0] - aux_vertex[0]), 2) + math.pow((vertex[1] - aux_vertex[1]), 2)))
+                low_distances.append((distance, vertex, aux_vertex))
         
         i = 1
         count = 0
@@ -30,42 +34,66 @@ def create_graph (v, k):
 
     return (list_vertices, list_edges)
 
-def deep_search(graph, initial_v, end_v, past_vertices):
-    print(f"Entrei no vertice {initial_v}")
-    edges = get_edges(graph,initial_v)
-    print(f"Edges {edges} \n\n")
-    past_vertices.append(initial_v)
-    if(end_v == initial_v): 
-        print(f"Achei final {end_v}")
-        return 1
-    for edge in edges:
-        if(edge[2] not in past_vertices):
-            print(f"Vou para o vertice {edge[2]}")
-            found = deep_search(graph, edge[2],end_v, past_vertices)
-            if(found): 
-                return 1
-            print(f"Voltei para o vertice {initial_v}")
-    return 0
-
-
 def get_edges(graph, vertex):
     edges = []
     for edge in graph[1]:
         if(edge[1] == vertex ):
             edges.append(edge)
     return edges
-    
 
-graph = create_graph(500,3)
+def DFS(graph, initial_v, end_v, past_vertices):
+    # print(initial_v)
+    edges = get_edges(graph,initial_v)
+    past_vertices.append(initial_v)
+    if(end_v == initial_v): 
+        print(f"Achei final (deep Search) {end_v}")
+        return 1
+    for edge in edges:
+        if(edge[2] not in past_vertices):
+            found = DFS(graph, edge[2],end_v, past_vertices)
+            if(found): 
+                return 1
+    return 0
+
+def BFS (graph, index_initial, index_end):
+    visited = [False] * (len(graph[0]))
+    vertex = graph[0][index_initial]
+    vertex_end = graph[0][index_end]
+    queue = [] 
+    queue.append(vertex)
+    visited[index_initial] = True
+    while queue: 
+        vertex = queue.pop(0) 
+        # print(vertex, " ")
+        edges = get_edges(graph,vertex)
+        for edge in edges:
+            index = graph[0].index(edge[2])
+            if(vertex_end == vertex):
+                print(f"Achei final (BFS) {vertex}")
+                return 1
+            if visited[index] == False: 
+                queue.append(edge[2]) 
+                visited[index] = True
+    return 0
+
+
+graph = create_graph(1000,7)
 past_vertices = []
-# print(graph[0])
-# print('-------')
-print(graph[1])
 
-print(graph[0][0])
-print(graph[0][3])
+print(f"Inicio: {graph[0][0]}, fim: {(graph[0][3])}")
 
-search = deep_search(graph, graph[0][0], graph[0][3], past_vertices)
+start = time.time()
+search = DFS(graph, graph[0][0], graph[0][3], past_vertices)
+end = time.time()
+print(end - start)
+
+
+print(f"Inicio: {graph[0][0]}, fim: {(graph[0][3])}")
+start = time.time()
+search = BFS (graph, 0, 3)
+end = time.time()
+print(end - start)
+
 
 
 
