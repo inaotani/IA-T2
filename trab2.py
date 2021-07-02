aa7b240613bdd6d06a96aac1a44c1f2904b97380
Gabriela Vidal <ga28bvidal@gmail.com>
2021-07-01 23:09:29 -0300

    algoritmo A* add

--- trab2.py
+++ trab2.py
@@ -2,7 +2,6 @@ import random
 import math
 import time
 
-
 def create_graph (v, k):
     list_vertices = []
     list_edges = []
@@ -76,17 +75,75 @@ def BFS (graph, index_initial, index_end):
                 visited[index] = True
     return 0
 
+def func_euclidiana(inicio, fim):   #Função que retorna a distância euclidiana dos parâmetros pedidos
+    dist_euclid = math.dist(inicio, fim)
+    return dist_euclid
+
+def find_best_index(lista, heuristica):
+    menor_valor = 10000000
+    melhor_opcao = None
+    for i in range(0, len(lista)):
+        if heuristica[lista[i]] < menor_valor:
+            menor_valor = heuristica[lista[i]]
+            melhor_opcao = i
+
+    #print(f"Melhor opção é: {lista[melhor_opcao]}")
+    return melhor_opcao
+
+def printar_resultado(caminho, inicio, fim):
+    print("Resultado da busca A*:")
+    print(f"Estado inicial: {inicio}    Estado final: {fim}")
+    print(f"Caminho percorrido: {caminho}")
+    print(f"Tamanho do caminho percorrido: {len(caminho)}")
+    return
+
+def alg_A_estrela(graph, inicio, fim):
+    distancia_percorrida = {}
+    euclidiana = {}
+    heuristica = {}   #h=distancia_percorrida+distancia_euclidiana
+    lista = []      #Lista com todos os vértices que foram encontrados
+    caminho = []    #Lista de caminhos já percorridos pelo programa
+
+    #inicializa as variáveis com valores corretos
+    lista.append(inicio)
+    euclidiana[inicio] = func_euclidiana(inicio, fim)
+    distancia_percorrida[inicio] = 0
+    heuristica[inicio] = distancia_percorrida[inicio] + euclidiana[inicio]
+
+    while len(lista)!=0:
+        #print(f"lista = {lista}")
+        best_index = find_best_index(lista, heuristica)
+        indice_at = lista.pop(best_index)
+        caminho.append(indice_at)
+
+        if(fim == indice_at):
+            printar_resultado(caminho, inicio, fim)
+            return 1
+        
+        edges = get_edges(graph, indice_at)
+        for edge in edges:
+            if edge[2] not in lista and edge[2] not in caminho: #Para conferir que seja analisado somente 1 vez cada nó
+                lista.append(edge[2])
+                distancia_percorrida[edge[2]] = distancia_percorrida[indice_at] + func_euclidiana(indice_at, edge[2])
+                euclidiana[edge[2]] = func_euclidiana(edge[2], fim)
+                heuristica[edge[2]] = euclidiana[edge[2]] + distancia_percorrida[edge[2]]
+                #print(f"analisando nó atual: {edge[2]} --> heuristica= {heuristica[edge[2]]}\n")
+    
+    print("Caminho não encontrado!!")
+
+    return 1
+
 
 graph = create_graph(1000,7)
 past_vertices = []
 
 print(f"Inicio: {graph[0][0]}, fim: {(graph[0][3])}")
+#print(f"Grafo: {graph}\n")
 
 start = time.time()
 search = DFS(graph, graph[0][0], graph[0][3], past_vertices)
 end = time.time()
 print(end - start)
-
 
 print(f"Inicio: {graph[0][0]}, fim: {(graph[0][3])}")
 start = time.time()
@@ -94,6 +151,11 @@ search = BFS (graph, 0, 3)
 end = time.time()
 print(end - start)
 
+print(f"Inicio: {graph[0][0]}, fim: {(graph[0][3])}")
+start = time.time()
+search = alg_A_estrela (graph, graph[0][0], graph[0][3])
+end = time.time()
+print(end - start)
 
 
 
